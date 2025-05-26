bec530ddd7fd94488884cfd628000988007bc428
Jerome <f77665532166@gmail.com>
2025-05-26 16:22:19 +0800

    explanation adding

---
+++ read.py
@@ -0,0 +1,15 @@
+import json
+
+with open('new_data/taiwan_bbc_news.json', 'r', encoding='utf-8') as f:
+    real_examples = json.load(f)
+
+with open('fake_data_test/output.json', 'r', encoding='utf-8') as f:
+    fake_examples = json.load(f)
+
+for fake_example in fake_examples:
+    for real_example in real_examples:
+        if fake_example['id'] == real_example['id']:
+            print(real_example)
+    print("============================================")
+    print(fake_example)
+    input()
