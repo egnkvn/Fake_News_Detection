ecbc737dd502b070407024622e0af5b437d761b1
Jerome <f77665532166@gmail.com>
2025-05-26 21:10:37 +0800

    demo2.json

--- read.py
+++ read.py
@@ -1,9 +1,9 @@
 import json
 
-with open('new_data/taiwan_bbc_news.json', 'r', encoding='utf-8') as f:
+with open('new_data/international_bbc_news.json', 'r', encoding='utf-8') as f:
     real_examples = json.load(f)
 
-with open('fake_data_test/output.json', 'r', encoding='utf-8') as f:
+with open('fake_data_test/demo2.json', 'r', encoding='utf-8') as f:
     fake_examples = json.load(f)
 
 for fake_example in fake_examples:
